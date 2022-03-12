7528392ad4939ae35b9accf3112c26af5c446e12
Fernanda <f_ernanda@outlook.com.br>
2022-03-11 22:59:34 -0300

    Change header logic

--- frontend/src/components/Hero/index.tsx
+++ frontend/src/components/Hero/index.tsx
@@ -2,9 +2,15 @@ import './styles.css'
 
 function Hero() {
     return (
-        <div className="fs-hero">
-            <h1 className="fs-main-title">Rate your favourite <span className="fs-title-emphasis">movies</span></h1>
-        </div>
+        <header className="fs-hero">
+            <nav>
+                <a className="fs-logo" href="/" translate="no">Movie App</a>
+            </nav>
+            <div className="fs-main-title-container">
+                <h1 className="fs-main-title">Rate your favourite <span className="fs-title-emphasis">movies</span></h1>
+            </div>
+        </header>
+
     )
 }
 
--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,6 +1,10 @@
+import Header from "@/components/Header"
+
 function Form() {
     return (
-        <h1>Form</h1>
+        <>
+            <Header/>
+        </>
     )
 }
 
